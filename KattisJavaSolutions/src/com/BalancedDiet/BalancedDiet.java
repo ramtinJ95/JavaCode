7dc01383e3156c356033d450a9215b8343e2b058
ramtinj <ramtinj@kth.se>
2017-07-11 22:21:33 +0200

    solved balancedDiet problem

--- KattisJavaSolutions/src/com/BalancedDiet/BalancedDiet.java
+++ KattisJavaSolutions/src/com/BalancedDiet/BalancedDiet.java
@@ -1,40 +1,58 @@
 package com.BalancedDiet;
 
-import java.util.ArrayList;
-import java.util.Hashtable;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by Ramtin on 2017-06-24.
  */
 public class BalancedDiet {
+    static int[][] K;
 
-    public Hashtable<Integer, ArrayList<Integer>> input = new Hashtable<>();
-    public String data = " ";
-    public ArrayList<Integer> keys = new ArrayList<>();
+    public static int calculate(int[] nums , int n , int W) {
 
+        for (int i = 0; i < K.length; i++)
+            for (int j = 0; j < K[0].length; j++)
+            {
+                if (i == 0 || j == 0)
+                    K[i][j] = 0;
 
-    public BalancedDiet(){
+                else if (nums[i - 1] > j)
+                    K[i][j] = K[i - 1][j];
 
+                else
+                    K[i][j] = Math.max(K[i - 1][j] , nums[i - 1] + K[i - 1][j - nums[i - 1]]);
+            }
+
+        return K[n][W];
     }
-    public void readIn(){
-        Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> cans = new ArrayList<>();
-        data = sc.nextLine();
-        String[] tokens = data.split(" ");
-        for(int i = 1; i < tokens.length; i++){
-            cans.add(Integer.parseInt(tokens[i]));
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        while (true)
+        {
+            int n = scan.nextInt();
+
+            if (n == 0)
+                break;
+
+            int[] cals = new int[n];
+            int sum = 0;
+
+            for (int i = 0; i < cals.length; i++)
+            {
+                cals[i] = scan.nextInt();
+                sum += cals[i];
+            }
+
+            K = new int[n + 1][sum / 2 + 1];
+
+            int small = calculate(cals , n , sum / 2);
+            int big = sum - small;
+
+            System.out.println(big + " " + small);
         }
-        keys.add(Integer.parseInt(tokens[0]));
-        input.put(Integer.parseInt(tokens[0]), cans);
+
+        scan.close();
     }
-
-    public void solve(Hashtable<Integer, ArrayList<Integer>> input ){
-        int answer;
-        int sum;
-        
-
-
-    }
-
 }
